7e509014d4dd409c4f982a31580b7cb877d572e5
Mikhail Burakov <mikhail.burakov@braingarden.ai>
2020-08-31 19:33:06 +0300

    Changes for BGRobot

--- pysot/tracker/siamrpn_tracker.py
+++ pysot/tracker/siamrpn_tracker.py
@@ -101,7 +101,7 @@ class SiamRPNTracker(SiameseTracker):
         h_z = self.size[1] + cfg.TRACK.CONTEXT_AMOUNT * np.sum(self.size)
         s_z = np.sqrt(w_z * h_z)
         scale_z = cfg.TRACK.EXEMPLAR_SIZE / s_z
-        s_x = s_z * (cfg.TRACK.INSTANCE_SIZE / cfg.TRACK.EXEMPLAR_SIZE)
+        s_x = s_z * (float(cfg.TRACK.INSTANCE_SIZE) / float(cfg.TRACK.EXEMPLAR_SIZE))
         x_crop = self.get_subwindow(img, self.center_pos,
                                     cfg.TRACK.INSTANCE_SIZE,
                                     round(s_x), self.channel_average)
@@ -160,3 +160,115 @@ class SiamRPNTracker(SiameseTracker):
                 'bbox': bbox,
                 'best_score': best_score
                }
+
+
+class SiamRPNTrackerMultiObject(SiamRPNTracker):
+
+    def __init__(self, model):
+        super(SiamRPNTrackerMultiObject, self).__init__(model)
+
+    def init(self, img, bbox):
+        """
+        args:
+            img(np.ndarray): BGR image
+            bbox: (x, y, w, h) bbox
+        """
+
+        tracking_object = {}
+
+        tracking_object['center_pos'] = np.array([bbox[0]+(bbox[2]-1)/2,
+                                                  bbox[1]+(bbox[3]-1)/2])
+        tracking_object['size'] = np.array([bbox[2], bbox[3]])
+
+        # calculate z crop size
+        w_z = tracking_object['size'][0] + self.model.cfg.TRACK.CONTEXT_AMOUNT * np.sum(tracking_object['size'])
+        h_z = tracking_object['size'][1] + self.model.cfg.TRACK.CONTEXT_AMOUNT * np.sum(tracking_object['size'])
+        s_z = round(np.sqrt(w_z * h_z))
+
+        # calculate channle average
+        tracking_object['channel_average'] = np.mean(img, axis=(0, 1))
+
+        # get crop
+        z_crop = self.get_subwindow(img, tracking_object['center_pos'],
+                                    self.model.cfg.TRACK.EXEMPLAR_SIZE,
+                                    s_z, tracking_object['channel_average'])
+        # zf_ = self.model.zf
+        self.model.template(z_crop)
+        tracking_object['zf'] = self.model.zf
+        # self.model.zf = zf_
+        return tracking_object
+
+    def track(self, img, tracking_object):
+        """
+        args:
+            img(np.ndarray): BGR image
+        return:
+            bbox(list):[x, y, width, height]
+        """
+
+        w_z = tracking_object['size'][0] + self.model.cfg.TRACK.CONTEXT_AMOUNT * np.sum(tracking_object['size'])
+        h_z = tracking_object['size'][1] + self.model.cfg.TRACK.CONTEXT_AMOUNT * np.sum(tracking_object['size'])
+        s_z = np.sqrt(w_z * h_z)
+        scale_z = self.model.cfg.TRACK.EXEMPLAR_SIZE / s_z
+        s_x = s_z * (self.model.cfg.TRACK.INSTANCE_SIZE / self.model.cfg.TRACK.EXEMPLAR_SIZE)
+        x_crop = self.get_subwindow(img, tracking_object['center_pos'],
+                                    self.model.cfg.TRACK.INSTANCE_SIZE,
+                                    round(s_x), tracking_object['channel_average'])
+
+        self.model.zf = tracking_object['zf']
+        outputs = self.model.track(x_crop)
+
+        score = self._convert_score(outputs['cls'])
+        pred_bbox = self._convert_bbox(outputs['loc'], self.anchors)
+
+        def change(r):
+            return np.maximum(r, 1. / r)
+
+        def sz(w, h):
+            pad = (w + h) * 0.5
+            return np.sqrt((w + pad) * (h + pad))
+
+        # scale penalty
+        s_c = change(sz(pred_bbox[2, :], pred_bbox[3, :]) /
+                     (sz(tracking_object['size'][0]*scale_z, tracking_object['size'][1]*scale_z)))
+
+        # aspect ratio penalty
+        r_c = change((tracking_object['size'][0]/tracking_object['size'][1]) /
+                     (pred_bbox[2, :]/pred_bbox[3, :]))
+        penalty = np.exp(-(r_c * s_c - 1) * self.model.cfg.TRACK.PENALTY_K)
+        pscore = penalty * score
+
+        # window penalty
+        pscore = pscore * (1 - self.model.cfg.TRACK.WINDOW_INFLUENCE) + \
+            self.window * self.model.cfg.TRACK.WINDOW_INFLUENCE
+        best_idx = np.argmax(pscore)
+
+        bbox = pred_bbox[:, best_idx] / scale_z
+        lr = penalty[best_idx] * score[best_idx] * self.model.cfg.TRACK.LR
+
+        cx = bbox[0] + tracking_object['center_pos'][0]
+        cy = bbox[1] + tracking_object['center_pos'][1]
+
+        # smooth bbox
+        width = tracking_object['size'][0] * (1 - lr) + bbox[2] * lr
+        height = tracking_object['size'][1] * (1 - lr) + bbox[3] * lr
+
+        # clip boundary
+        cx, cy, width, height = self._bbox_clip(cx, cy, width,
+                                                height, img.shape[:2])
+
+        # udpate state
+        tracking_object['center_pos'] = np.array([cx, cy])
+        tracking_object['size'] = np.array([width, height])
+
+        bbox = [cx - width / 2,
+                cy - height / 2,
+                width,
+                height]
+        best_score = score[best_idx]
+        return {
+                'bbox': bbox,
+                'best_score': best_score
+               }
+
+
--- pysot/tracker/tracker_builder.py
+++ pysot/tracker/tracker_builder.py
@@ -6,12 +6,13 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from pysot.core.config import cfg
-from pysot.tracker.siamrpn_tracker import SiamRPNTracker
+from pysot.tracker.siamrpn_tracker import SiamRPNTracker, SiamRPNTrackerMultiObject
 from pysot.tracker.siammask_tracker import SiamMaskTracker
 from pysot.tracker.siamrpnlt_tracker import SiamRPNLTTracker
 
 TRACKS = {
           'SiamRPNTracker': SiamRPNTracker,
+          'SiamRPNTrackerMultiObject': SiamRPNTrackerMultiObject,
           'SiamMaskTracker': SiamMaskTracker,
           'SiamRPNLTTracker': SiamRPNLTTracker
          }
